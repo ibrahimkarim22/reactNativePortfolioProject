0d6de1d54d063fb68d2efb5d886209c1bbf168f0
karim <hema_thechamp@yahoo.com>
2024-05-05 15:21:45 -0400

    added .env file by installing reactnativedotenv and updating the pluggins in the babelconfig also added another file to make the env work in ts environemnt in that file i defnied the type string for the content in the env file

---
+++ firebaseConfig.ts
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { initializeAuth, getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// import { getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from "@env"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP)
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP)
+
+

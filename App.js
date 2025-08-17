788538452f12ca86fd301330a35dd8ea06c19b96
karim <hema_thechamp@yahoo.com>
2025-08-17 13:33:10 -0400

    Bard will be the first app I launch to the Play and App stores. I am starting to fix a few things and revisit every element to make it launch ready. I started with firebaseconfig and app js by cleaning it up and adding persistence

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import React from "react";
-import { StatusBar } from "react-native";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import Stacks from "./Componenets/Navigation";
-import { initializeApp } from "@react-native-firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import { FIREBASE_APP, FIREBASE_AUTH, firebaseConfig } from "./firebaseConfig";
 import { PersistGate } from "redux-persist/integration/react";
-import { Text } from "react-native-elements";
 import { NavigationContainer } from "@react-navigation/native";
+
+import { StatusBar } from "react-native";
+// import { Text } from "react-native-elements";
+
+import "./firebaseConfig";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
+      {/* <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}> */}
+      <PersistGate loading={null} persistor={persistor}>
         <NavigationContainer>
           <StatusBar backgroundColor="black" barStyle="light-content" />
           <Stacks />
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { initializeAuth, getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID, MEASUREMENT_ID } from '@env';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from "@env";
 
-// import { getReactNativePersistence } from "firebase/auth";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: API_KEY,
   authDomain: AUTH_DOMAIN,
@@ -21,8 +23,9 @@ export const firebaseConfig = {
   measurementId: MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP)
-export const FIRESTORE_DB = getFirestore(FIREBASE_APP)
 
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
